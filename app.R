195245d1df0fa01706267dd5dce672a2ab283762
RamiKrispin <ramkrisp@umich.edu>
2025-03-12 21:06:23 -0700

    updated the app

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ ts <- read.csv(file = "https://raw.githubusercontent.com/RamiKrispin/ts-cluster-
     tsibble::as_tsibble(index = index, key = c(area_name, process_name))
 features <- read.csv(file = "https://raw.githubusercontent.com/RamiKrispin/ts-cluster-analysis-r/refs/heads/main/data/features.csv")
 
-
+= 
 features_list <- names(features)
 
 features_list <- features_list[-which(features_list %in% c("process", "PC1", "PC2", "PC3", "process", "area_name"))]
@@ -287,3 +287,5 @@ shinyApp(ui = ui, server = server)
 
 
 # shinylive::export(appdir = ".", destdir = "docs", package_cache = FALSE, wasm_packages =  FALSE)
+
+x = 1
